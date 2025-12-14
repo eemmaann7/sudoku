4e09156aae1775338859727fce967b075606deb6
eemmaann7 <eman@gmail.com>
2025-12-14 16:21:17 +0300

    basic-board

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+
 let timerInterval
 let seconds = 0
 let mistakes = 0
+
+var numSelected = null
+var tileSelected = null
 
 const board = document.getElementById("grid-board")
 
@@ -9,53 +13,7 @@ window.onload = function () {
 }
 
 function setGame() {
-  let boardEl = document.getElementById("grid-board")
-  boardEl.innerHTML = ""
- 
-  // Reset
-  seconds = 0
-  mistakes = 0
-  gameStarted = false
-  gameOver = false
-
-  document.getElementById("timer").innerText = "00:00"
-  document.getElementById("mistakes").innerText = "0/3"
-  clearInterval(timerInterval)
-  
-  // Load puzzle
-  let difficulty = document.getElementById("difficulty").value
-
-  // Create board tiles/cells
-   for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div")
-            // "row - colom"
-            tile.id = r.toString() + "-" + c.toString()
-            tile.addEventListener("click", selectTile)
-            tile.classList.add("tile")
-            document.getElementById("grid-board").append(tile)
-        }
-    }
-
-
-  // Buttons
-  document.getElementById("newBtn").onclick = setGame
-
-  document.getElementById("pauseBtn").onclick = function () {
-    if (gameOver) return 
-
-    if (timerInterval) {
-      clearInterval(timerInterval)
-      timerInterval = null
-      this.innerText = "Resume"
-    } else {
-      startTimer();
-      this.innerText = "Pause"
-    }
-  }
-}
-
-// Create loop for digits (1–9)
+  // Create loop for digits (1–9)
   for (let i = 1; i <= 9; i++) {
     let number = document.createElement("div")
     number.classList.add("number")
@@ -63,18 +21,14 @@ function setGame() {
     document.getElementById("digits").appendChild(number);
   }
 
-// TIMER
-function startTimer() {
-  timerInterval = setInterval(() => {
-    seconds++
-    let mins = String(Math.floor(seconds / 60)).padStart(2, "0")
-    let secs = String(seconds % 60).padStart(2, "0")
-    document.getElementById("timer").innerText = `${mins}:${secs}`
-  }, 1000)
+  // Create board 9x9 tiles/cells
+   for (let row = 0; row < 9; row++) {
+        for (let colom = 0; colom < 9; colom++) {
+            let tile = document.createElement("div")
+            // "row - colom"
+            tile.id = row.toString() + "-" + colom.toString()
+            tile.classList.add("tile")
+            document.getElementById("grid-board").append(tile)
+        }
+    }
 }
-function endGame() {
-  gameOver = true
-  clearInterval(timerInterval)
-  alert("Game Over ! ")
-}
-
